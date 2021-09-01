44de376b959ce128d630e8f83c13fa651757cde8
sjgridley <88527083+sjgridley@users.noreply.github.com>
2021-09-01 20:51:19 +1000

    change date

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    established_date = '20th March 1972'
+def index():
+    established_date = '21st March 1970'
     return render_template('index.html', established_date = established_date)
